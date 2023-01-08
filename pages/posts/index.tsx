19f88cbc2695b23b0e4f6c6134536df213bdbcac
Mani Movassagh <Mani.mgh@gmail.com>
2023-01-09 00:12:41 +0100

    handle create

---
+++ pages/posts/index.tsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Post() {
+  const [token, setToken] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    setToken(localStorage.getItem("token") as string);
+  }, []);
+  const handleTitleChange = (event: any) => {
+    setTitle(event.target.value);
+  };
+  const handleDescriptionChange = (event: any) => {
+    setDescription(event.target.value);
+  };
+  const handleContentChange = (event: any) => {
+    setContent(event.target.value);
+  };
+
+  const handleCreatePost = (event: any) => {
+    event.preventDefault();
+    console.log(content, description, title);
+    console.log("handle create is working");
+  };
+
+  return (
+    <div className="mt-12">
+      <div className="text-center text-2xl mb-4 text-blue-500">Create Post</div>
+      <form className="mx-auto max-w-sm">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="title"
+          >
+            Post Title
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="title"
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={handleTitleChange}
+          ></input>
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="description"
+          >
+            Post Description
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="description"
+            type="text"
+            placeholder="Description"
+            value={description}
+            onChange={handleDescriptionChange}
+          ></input>
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="content"
+          >
+            Post Content
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="content"
+            type="text"
+            placeholder="Content"
+            value={content}
+            onChange={handleContentChange}
+          ></input>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+            onClick={() => handleCreatePost(event)}
+          >
+            Create Post
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
