a2a32d85aaebf32621858893bf6b166789f9bf75
Mani Movassagh <Mani.mgh@gmail.com>
2023-01-08 22:38:50 +0100

    get access token from backend

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -5,54 +5,78 @@ interface FormProps {
 }
 
 const Form: FunctionComponent<FormProps> = ({ onSubmit }) => {
-  const [first, setFirst] = useState("");
-  const [last, setLast] = useState("");
+  const [usernameOrEmail, setUsernameOrEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleNameChange = (event: any) => {
+    setUsernameOrEmail(event.target.value);
+  };
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: any) => {
     event.preventDefault();
-    onSubmit(first, last);
+    console.log(usernameOrEmail, password);
+
+    const data = { usernameOrEmail, password };
+
+    fetch("http://localhost:8085/api/v1/auth/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      });
   };
 
   return (
-    <div className="shadow-md w-1/2 mx-auto mt-24">
-      <form
-        className="flex flex-col items-center justify-center w-1/3 mx-auto p-4"
-        onSubmit={handleSubmit}
-      >
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="first"
-        >
-          First name:
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          id="first"
-          name="first"
-          value={first}
-          onChange={(event) => setFirst(event.target.value)}
-        />
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2 mt-4"
-          htmlFor="last"
-        >
-          Last name:
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          id="last"
-          name="last"
-          value={last}
-          onChange={(event) => setLast(event.target.value)}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-          type="submit"
-        >
-          Submit
-        </button>
+    <div className="mt-12">
+      <div className="text-center text-2xl mb-4 text-blue-500">Login Form</div>
+      <form className="mx-auto max-w-sm">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Name or Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Name"
+            value={usernameOrEmail}
+            onChange={handleNameChange}
+          ></input>
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="text"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          ></input>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+            onClick={() => handleSubmit(event)}
+          >
+            Login
+          </button>
+        </div>
       </form>
     </div>
   );
